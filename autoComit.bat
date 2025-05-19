4a1ad416eb730b1931d75bf52068b11a13e3ca6c
Anatolie Jentimir <157539682+jentimanatol@users.noreply.github.com>
2025-05-18 20:02:31 -0400

    'UPDATE ICO IN TASCK BAR ico size max 250*250 '

--- autoComit.bat
+++ autoComit.bat
@@ -1,9 +1,9 @@
 git --version
 git add .
-git commit -m "'UPDATE ICO IN TASCK BAR '"
+git commit -m "'UPDATE ICO IN TASCK BAR ico size max 250*250 '"
 git push origin main
 
 :: === Tagging for GitHub Actions Release Build ===
-git tag v1.1
-git push origin v1.1
+git tag v1.2
+git push origin v1.2
 pause
